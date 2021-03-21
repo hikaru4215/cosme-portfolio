d4f5927631aa44df4dca74a2d73ff293025871d9
hikaru4215 <hikaru4215hkr2178ay@yahoo.co.jp>
2021-03-21 22:44:14 +0900

    settings修正

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'widget_tweaks', 
+    'widget_tweaks',
     'app', 
     'accounts', 
     'django.contrib.sites', 
